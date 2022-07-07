24617a7a05f5f06622db359480d3aa688dcf636c
ge38loy <ge38loy@mytum.de>
2022-07-07 15:41:24 +0200

    Created a Friend class in FriendshipHandler

--- Assets/Scripts/Social/FriendshipHandler.cs
+++ Assets/Scripts/Social/FriendshipHandler.cs
@@ -1,10 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FriendshipHandler : MonoBehaviour
 {
-    
+    class Friend : IComparable
+    {
+        public string name;
+        public int level;
+        public Friend(string name, int level)
+        {
+            this.name = name;
+            this.level = level;
+        }
+		
+        public int CompareTo(object obj) {
+            if (obj == null) return 1;
+
+            Friend otherFriend = obj as Friend;
+            if (otherFriend != null)
+                return otherFriend.level.CompareTo(this.level);
+            else
+                throw new ArgumentException("Object is not a Friend");
+        }
+    }
+        
     void AddFriend()
     {
         
