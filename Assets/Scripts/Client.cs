5921078beee8a605b8b486a7453d4602bc82e608
ge38loy <ge38loy@mytum.de>
2022-07-07 13:15:36 +0200

    first tweaks at the client

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Client
+{
+    public const string BASE_URL = "http://127.0.0.1:8000/gameserver/";
+    public static string username = "";
+}
