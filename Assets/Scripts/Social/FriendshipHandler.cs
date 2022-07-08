30d5ff15f129535db497d81f4541b906a56d6564
ge38loy <ge38loy@mytum.de>
2022-07-08 13:23:00 +0200

    added a generic Uploader and a generic Downloader to the Client class

--- Assets/Scripts/Social/FriendshipHandler.cs
+++ Assets/Scripts/Social/FriendshipHandler.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FriendshipHandler : MonoBehaviour
 {
-    class Friend : IComparable
+    class Friend
     {
         public string name;
         public int level;
@@ -14,20 +11,15 @@ public class FriendshipHandler : MonoBehaviour
             this.name = name;
             this.level = level;
         }
-		
-        public int CompareTo(object obj) {
-            if (obj == null) return 1;
-
-            Friend otherFriend = obj as Friend;
-            if (otherFriend != null)
-                return otherFriend.level.CompareTo(this.level);
-            else
-                throw new ArgumentException("Object is not a Friend");
-        }
     }
         
-    void AddFriend()
+    void CallAddFriend(string username)
     {
-        
+        StartCoroutine(Client.UploadRequest("name", username, Client.SEND_FRIEND_REQUEST));
+    }
+
+    void CallAcceptFriend(string username)
+    {
+        StartCoroutine(Client.UploadRequest("name", username, Client.ACCEPT_FRIEND_REQUEST));
     }
 }
